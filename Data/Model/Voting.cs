cf6b7ca96951f9b1e59068b7fafbcc4990ac8237
Jecs <john024996@gmail.com>
2023-10-28 23:47:16 -0400

    add new column

--- Data/Model/Voting.cs
+++ Data/Model/Voting.cs
@@ -4,8 +4,9 @@
     {
         public int? Id { get; set; }
         public string? Name { get; set; }
+        public string? LastName { get; set; }
         public string? VatId { get; set; }
-        public string? Census { get; set; }
+        public string? Enclosure { get; set; }
         public string? School { get; set; }
         public Boolean IsRegistered { get; set; }
 
---
+++ Migrations/20231028215906_AddNewColumn.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElectLive_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Census",
+                table: "Votings",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Enclosure",
+                table: "Votings",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Enclosure",
+                table: "Votings");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Votings",
+                newName: "Census");
+        }
+    }
+}
